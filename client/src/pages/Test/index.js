98dabfab749a26019e18ab6995a03c03c90a9809
violet chang <violet7chang@gmail.com>
2019-09-22 19:06:19 -0500

    css updates

--- client/src/pages/Test/index.js
+++ client/src/pages/Test/index.js
@@ -6,7 +6,7 @@ import API from '../../utils/API';
 import Container from '../../components/Container';
 import RegisterForm from '../../components/RegisterForm';
 import Card from '../../components/Card';
-import Button from '../../components/Button';
+// import Button from '../../components/Button';
 
 
 class Test extends React.Component {
@@ -58,7 +58,7 @@ class Test extends React.Component {
                         />
                     </Card>
                 </Container>
-                <Button />
+                {/* <Button /> */}
             </div>
         );
     }
