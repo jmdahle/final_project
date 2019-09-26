ef8b31218a9092883a43a4b55c16ba280a0df1ca
Andrew P <andrewpetersondev@gmail.com>
2019-09-26 12:37:03 -0500

    removed placeholder cards

--- client/src/pages/Manage/index.js
+++ client/src/pages/Manage/index.js
@@ -10,7 +10,7 @@ import GoalCard from "../../components/GoalCard";
 class Manage extends React.Component {
   render() {
     return (
-      <div className="manage">
+      <div className="manage-page">
         <Container className="active-goals-section">
           <h2>Active Goals</h2>
           <div>
@@ -26,12 +26,23 @@ class Manage extends React.Component {
               <span>No goals in this section.</span>
             )}
           </div>
-          <GoalCard />
         </Container>
         <hr />
         <Container className="achieved-goals-section">
           <h2>Achieved Goals</h2>
-          <GoalCard />
+          <div>
+            {this.props.goals.length > 0 ? (
+              this.props.goals.map(goal => (
+                <GoalCard
+                  id={goal._id}
+                  key={goal._id}
+                  goalName={goal.goalName}
+                />
+              ))
+            ) : (
+              <span>No goals in this section.</span>
+            )}
+          </div>
         </Container>
       </div>
     );
