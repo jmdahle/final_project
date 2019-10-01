ee8d0b14170b6e444a4c3c3e3d2b916f62559619
Andrew P <andrewpetersondev@gmail.com>
2019-09-30 20:47:45 -0500

    progress bar of user goal is connected to user goal percent value

--- client/src/components/UserGoalCard/index.js
+++ client/src/components/UserGoalCard/index.js
@@ -3,14 +3,14 @@ import ProgressBar from 'react-bootstrap/ProgressBar';
 // import Card from 'react-bootstrap/Card';
 import './style.css';
 
-const UserGoalCard = ({ selectGoal, goalName, id }) => {
+const UserGoalCard = ({ selectGoal, goalName, id, goalPercent }) => {
   return (
     <div className="user-goal-card" onClick={() => selectGoal(id)}>
       <div className="user-goal-card-header">
         <h5>{goalName}</h5>
       </div>
       <div className="user-goal-card-body">display visual details here.</div>
-      <ProgressBar variant="success" now={40} />
+      <ProgressBar variant="success" now={goalPercent} />
     </div>
   );
 };
