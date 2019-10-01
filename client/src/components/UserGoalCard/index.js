98318f1b76d0f775da1d5110bb9fd2da1fb96ecd
Andrew P <andrewpetersondev@gmail.com>
2019-10-01 10:31:10 -0500

    style changes

--- client/src/components/UserGoalCard/index.js
+++ client/src/components/UserGoalCard/index.js
@@ -1,17 +1,35 @@
 import React from "react";
 import ProgressBar from 'react-bootstrap/ProgressBar';
-// import Card from 'react-bootstrap/Card';
-import './style.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+// import './style.css';
 
 const UserGoalCard = ({ selectGoal, goalName, id, goalPercent }) => {
   return (
-    <div className="user-goal-card" onClick={() => selectGoal(id)}>
-      <div className="user-goal-card-header">
-        <h5>{goalName}</h5>
-      </div>
-      <div className="user-goal-card-body">display visual details here.</div>
-      <ProgressBar variant="success" now={goalPercent} />
-    </div>
+    // <div className="user-goal-card" onClick={() => selectGoal(id)}>
+    //   <div className="user-goal-card-header">
+    //     <h5>{goalName}</h5>
+    //   </div>
+    //   <div className="user-goal-card-body">
+    //     <ProgressBar variant="success" now={goalPercent} />
+    //   </div>
+    // </div>
+
+
+<Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="./images/sadBirthday.jpg" />
+  <Card.Body>
+    <Card.Title>{goalName}</Card.Title>
+    <Card.Text>
+    shame, shame, shame on you
+    </Card.Text>
+    <ProgressBar variant="success" now={goalPercent} />
+    <br />
+    <Button variant="primary">See Tasks!</Button>
+  </Card.Body>
+</Card>
+
+
   );
 };
 
--- client/src/pages/Manage/index.js
+++ client/src/pages/Manage/index.js
@@ -1,9 +1,14 @@
 // react
 import React from "react";
 
+// react bootstrap
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 // components
-import Container from "../../components/Container";
-import { Col, Row } from "../../components/Grid";
+// import Container from "../../components/Container";
+// import { Col, Row } from "../../components/Grid";
 import UserGoalCard from "../../components/UserGoalCard";
 // import GoalCard from '../../components/GoalCard';
 
@@ -15,9 +20,9 @@ class Manage extends React.Component {
     return (
       <Container>
         <Row>
-          <Col size="md-12">
+          <Col size="xs-12 md-8">
              <h2>Active Goals</h2> 
-            <div>
+             <div>
               {this.props.userGoals ? (
                 this.props.userGoals.map(userGoal => (
                   <UserGoalCard
@@ -25,6 +30,8 @@ class Manage extends React.Component {
                     key={userGoal._id}
                     goalName={userGoal.goalName}
                     goalPercent={userGoal.goalPercent}
+                    // showTaskOverlay={this.state.showTaskOverlay}  
+                    // tasks={this.state.tasks}
                   />
                 ))
               ) : (
