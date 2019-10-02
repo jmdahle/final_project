f89e1e8287066c2e33588a50b2378a621b54fdec
JMDahle <47642064+jmdahle@users.noreply.github.com>
2019-10-01 19:12:02 -0500

    added email to new registration; fixed register page (closes login window); addgoal now goes to manage page

--- controllers/emailController.js
+++ controllers/emailController.js
@@ -3,8 +3,8 @@ const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-        user: 'planetdahlelama@gmail.com',
-        pass:  'yybarnidyasoluag' // application-specific
+        user: 'keepittogetherteam@gmail.com',
+        pass:  'ckqdqorvvvjqflax' // application-specific
     }
 });
 
@@ -12,16 +12,18 @@ module.exports = {
     email: function(request,response) {
         console.log('sending an email');
         let mailOptions = {
-            from: 'planetdahlelama@gmail.com',
+            from: 'keepittogetherteam@gmail.com',
             to: request.body.user_email,
             subject: 'Welcome to KIT',
-            text: 'Welcome to Keep It Together, knuckleheadd.'
+            text: 'Welcome to Keep It Together, Knucklehead.'
         };
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log(error);
+                response.sendStatus(500);
             } else {
                 console.log('Email sent' + info.response);
+                response.sendStatus(200);
             }
         });
     }
